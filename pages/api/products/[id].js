ba5625b2e94f5433776796d3cf9c362db5224f91
Denis <41701228+danmiles@users.noreply.github.com>
2022-07-12 00:55:18 +0200

    Delete ADD Product verification

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -7,7 +7,6 @@ export default async function handler(req, res) {
         query: { id },
         cookies,
     } = req;
-    const token = cookies.token;
 
     await dbConnect();
 
@@ -21,9 +20,6 @@ export default async function handler(req, res) {
     }
 
     if (method === "PUT") {
-        if (!token || token !== process.env.token) {
-            return res.status(401).json("Not authenticated!");
-        }
         try {
             const product = await Product.findByIdAndUpdate(id, req.body, {
                 new: true,
@@ -35,9 +31,6 @@ export default async function handler(req, res) {
     }
 
     if (method === "DELETE") {
-        if (!token || token !== process.env.token) {
-            return res.status(401).json("Not authenticated!");
-        }
         try {
             await Product.findByIdAndDelete(id);
             res.status(200).json("The product has been deleted!");
