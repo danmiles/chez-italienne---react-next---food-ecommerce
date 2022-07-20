d366afc2ae0dc9549c1f6fd0997f8f884fd35482
Denis <41701228+danmiles@users.noreply.github.com>
2022-07-20 13:40:11 +0200

    Update PizzaList.jsx

--- components/PizzaList.jsx
+++ components/PizzaList.jsx
@@ -4,12 +4,12 @@ import PizzaCard from "./PizzaCard";
 const PizzaList = ({ pizzaList }) => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Uniquement des produits italiens d'excellence</h1>
+      <h1 className={styles.title}>Uniquement des produits italiens dexcellence</h1>
       <p className={styles.desc}>
-        Nous vendons de la nourriture de la ferme italienne. Nous permettons à nos clients de découvrir la culture italienne et les valeurs familiales à travers nos produits.
+        Nous vendons de la nourriture de la ferme italienne. Nous permettons a nos clients de decouvrir la culture italienne et les valeurs familiales a travers nos produits.
       </p>
       <p className={styles.desc}>
-        Produits de la ferme italienne cultivés avec amour et soin.
+        Produits de la ferme italienne cultives avec amour et soin.
       </p>
       <div className={styles.wrapper}>
         {pizzaList.map((pizza) => (
