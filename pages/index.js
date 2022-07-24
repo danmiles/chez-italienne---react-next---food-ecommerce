8ca4cf219b2d49ff033b862c71426060ecc07b81
Denis <41701228+danmiles@users.noreply.github.com>
2022-07-24 11:16:47 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 import Head from "next/head";
 import Image from "next/image";
-import { useState } from "react";
 import Featured from "../components/Featured";
 import PizzaList from "../components/PizzaList";
 import styles from "../styles/Home.module.css";
 
-export default function Home({ pizzaList, admin }) {
-    const [close, setClose] = useState(true);
+export default function Home({ pizzaList }) {
     return (
         <div className={styles.container}>
             <Head>
@@ -27,13 +25,7 @@ export default function Home({ pizzaList, admin }) {
     );
 }
 
-export const getServerSideProps = async (ctx) => {
-    const myCookie = ctx.req?.cookies || "";
-    let admin = false;
-
-    if (myCookie.token === process.env.TOKEN) {
-        admin = true;
-    }
+export const getServerSideProps = async () => {
     // Deploy
     const res = await axios.get(
         "https://chez-italienne-react-next.vercel.app/api/products"
