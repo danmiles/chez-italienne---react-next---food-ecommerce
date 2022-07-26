ae915f72b931c0fdec5e03bc4940f11b9d6294cf
Denis <41701228+danmiles@users.noreply.github.com>
2022-07-26 22:38:39 +0200

    Images

--- components/PizzaList.jsx
+++ components/PizzaList.jsx
@@ -6,7 +6,8 @@ const PizzaList = ({ pizzaList }) => {
     <div className={styles.container}>
       <h1 className={styles.title}>Uniquement des produits italiens dexcellence</h1>
       <p className={styles.desc}>
-        Nous vendons de la nourriture de la ferme italienne. Nous permettons a nos clients de decouvrir la culture italienne et les valeurs familiales a travers nos produits.
+        Nous vendons de la nourriture de la ferme italienne. Nous permettons 
+        a nos clients de decouvrir la culture italienne et les valeurs familiales a travers nos produits.
       </p>
       <p className={styles.desc}>
         Produits de la ferme italienne cultives avec amour et soin.
